1c4003bb9519c950ecd54819e319b5c760fabc2a
Eric Chase <eric.nalley+github@gmail.com>
2019-02-27 01:39:06 -0600

    register service worker

--- public/main.js
+++ public/main.js
@@ -1,6 +1,11 @@
 // src/main.js
-import App from "./components/App.js";
+import App from "./components/app.js";
 
 new Vue({
   render: h => h(App)
 }).$mount(`#app`);
+
+// register the service-worker
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js');
+};
