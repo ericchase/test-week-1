3b43a83d7c798038b803500bb44677fc53c9d5f8
Eric Chase <eric.nalley+github@gmail.com>
2019-02-26 23:02:35 -0600

    updated app text

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+// src/components/App.js
+export default {
+  name: "App",
+  template: `
+      <div class="container mx-auto p-4">
+        <h1>Sellin' propane and propane accessories I tell you whut</h1>
+      </div>
+    `
+};
