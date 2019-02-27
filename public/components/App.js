feff8d085aa25ab00d98bb4acb3632e306bd4c5e
Eric Chase <eric.nalley+github@gmail.com>
2019-02-27 01:39:14 -0600

    minor rename

--- public/components/App.js
+++ public/components/App.js
@@ -1,6 +1,6 @@
-// src/components/App.js
+// src/components/app.js
 export default {
-  name: "App",
+  name: "app",
   template: `
       <div class="container mx-auto p-4">
         <h1>Sellin' propane and propane accessories I tell you whut</h1>
